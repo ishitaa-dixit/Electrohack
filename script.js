dbd267a62ec682d4cdd018c6f3a08d26b96d2025
Ishitaa <dixitishita10@gmail.com>
2023-11-29 03:50:41 +0530

    About page and addition in calender

---
+++ script.js
@@ -0,0 +1,47 @@
+const monthYearElement = document.getElementById('monthYear');
+const datesElement = document.getElementById('dates');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+let currentDate = new Date();
+const updateCalendar = () => {
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth();
+    const firstDay = new Date(currentYear, currentMonth, 1);
+    const lastDay = new Date(currentYear, currentMonth + 1, 0);
+    const totalDays = lastDay.getDate();
+    const firstDayIndex = firstDay.getDay();
+    const lastDayIndex = lastDay.getDay();
+
+    const monthYearString = currentDate.toLocaleString('default', { month: 'long', year: 'numeric' });
+    monthYearElement.textContent = monthYearString;
+
+    let dateHTML = '';
+    for (let i = 0; i < firstDayIndex; i++) {
+        dateHTML += `<div class="date inactive"></div>`;
+    }
+
+    for (let i = 1; i <= totalDays; i++) {
+        const date = new Date(currentYear, currentMonth, i);
+        const activeClass = date.toDateString() === new Date().toDateString() ? 'active' : '';
+        dateHTML += `<div class="date ${activeClass}">${i}</div>`;
+    }
+
+    for (let i = 1; i <= 6 - lastDayIndex; i++) {
+        dateHTML += `<div class="date inactive"></div>`;
+    }
+
+    datesElement.innerHTML = dateHTML;
+};
+
+prevBtn.addEventListener('click', () => {
+    currentDate.setMonth(currentDate.getMonth() - 1);
+    updateCalendar();
+});
+
+nextBtn.addEventListener('click', () => {
+    currentDate.setMonth(currentDate.getMonth() + 1);
+    updateCalendar();
+});
+
+updateCalendar();
