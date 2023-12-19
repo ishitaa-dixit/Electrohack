2ab73f9bc39e0f64cc763bcc4e89eff500832d85
Ishitaa <dixitishita10@gmail.com>
2023-12-19 12:35:49 +0530

    Js for habit.html

--- script.js
+++ script.js
@@ -72,3 +72,95 @@ nextBtn.addEventListener('click', () => {
 });
 
 updateCalendar();
+
+
+
+// IEFE
+(() => {
+    // state variables
+    let toDoListArray = [];
+    // ui variables
+    const form = document.querySelector(".form");
+    const input = form.querySelector(".form__input");
+    const ul = document.querySelector(".toDoList");
+  
+    // event listeners
+    form.addEventListener('submit', e => {
+      // prevent default behaviour - Page reload
+      e.preventDefault();
+      // give item a unique ID
+      let itemId = String(Date.now());
+      // get/assign input value
+      let toDoItem = input.value;
+      //pass ID and item into functions
+      addItemToDOM(itemId , toDoItem);
+      addItemToArray(itemId, toDoItem);
+      // clear the input box. (this is default behaviour but we got rid of that)
+      input.value = '';
+    });
+  
+    ul.addEventListener('click', e => {
+      let id = e.target.getAttribute('data-id')
+      if (!id) return // user clicked in something else
+      //pass id through to functions
+      removeItemFromDOM(id);
+      removeItemFromArray(id);
+    });
+  
+    // functions
+    function addItemToDOM(itemId, toDoItem) {
+      // create an li
+      const li = document.createElement('li')
+      li.setAttribute("data-id", itemId);
+      // add toDoItem text to li
+      li.innerText = toDoItem
+      // add li to the DOM
+      ul.appendChild(li);
+    }
+  
+    function addItemToArray(itemId, toDoItem) {
+      // add item to array as an object with an ID so we can find and delete it later
+      toDoListArray.push({ itemId, toDoItem});
+      console.log(toDoListArray)
+    }
+  
+    function removeItemFromDOM(id) {
+      // get the list item by data ID
+      var li = document.querySelector('[data-id="' + id + '"]');
+      // remove list item
+      ul.removeChild(li);
+    }
+  
+    function removeItemFromArray(id) {
+      // create a new toDoListArray with all li's that don't match the ID
+      toDoListArray = toDoListArray.filter(item => item.itemId !== id);
+      console.log(toDoListArray);
+    }
+  
+  })();
+
+
+function addEntry() {
+    // const dateInput = document.getElementById('entryDate');
+    const textInput = document.getElementById('entryText');
+    const entriesContainer = document.getElementById('entries');
+
+    const date = currentDate;
+    const text = textInput.value;
+
+    if (text) {
+        const entryElement = document.createElement('div');
+        entryElement.className = 'entry';
+        entryElement.innerHTML = `<p><strong>${date}</strong>: ${text}</p>`;
+        entriesContainer.appendChild(entryElement);
+
+        // Optionally, you can save the entries to localStorage or a server.
+        // For simplicity, this example does not include persistent storage.
+    } else {
+        alert('Please enter both date and journal entry.');
+    }
+
+    // Clear the form after adding an entry
+    // dateInput.value = '';
+    textInput.value = '';
+}
